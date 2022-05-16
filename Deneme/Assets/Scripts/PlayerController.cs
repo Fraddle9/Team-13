3413f00d465f4664707a8526872ac90811038862
Nuh Akturk <nuhar42@gmail.com>
2022-05-16 05:53:26 +0300

    2D Light eklendi ve proje URP yapildi

--- Deneme/Assets/Scripts/PlayerController.cs
+++ Deneme/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Camera
+    [SerializeField] GameObject _camera;
     //Movement
     [Header("Movement")]
     public float runSpeed;
@@ -66,6 +68,7 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         //Jump();
+        _camera.transform.position = new Vector3(transform.position.x + 2.0f, transform.position.y + 2.5f, transform.position.z - 1.0f);
     }
     void CheckState()
     {
